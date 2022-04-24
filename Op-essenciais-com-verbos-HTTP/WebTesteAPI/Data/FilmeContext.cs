d383b77411db0860743188c799f68dbe53182b7e
Rafael Souza <rafael_popo@hotmail.com>
2022-04-24 01:28:07 -0300

    Curso 01 - upload

---
+++ Op-essenciais-com-verbos-HTTP/WebTesteAPI/Data/FilmeContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebTesteAPI.Models;
+
+namespace WebTesteAPI.Data
+{
+    public class FilmeContext : DbContext
+    {
+        public FilmeContext(DbContextOptions<FilmeContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+    }
+}
---
+++ Op-essenciais-com-verbos-HTTP/WebTesteAPI/Profiles/FilmeProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebTesteAPI.Data.Dtos;
+using WebTesteAPI.Models;
+
+namespace WebTesteAPI.Profiles
+{
+    public class FilmeProfile : Profile
+    {
+        public FilmeProfile()
+        {
+            CreateMap<CreateFilmeDto, Filme>();
+            CreateMap<Filme, ReadFilmeDto>();
+            CreateMap<UpdateFilmeDto, Filme>();
+        }
+    }
+}
